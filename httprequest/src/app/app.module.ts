03f75375a34020406a72a419b693e4bf1193ff8a
lucatedesco80 <lucatedesco80@gmail.com>
2019-09-20 09:38:45 +0000

    httprequest

---
+++ httprequest/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponentComponent } from './foo/foo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooComponentComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
